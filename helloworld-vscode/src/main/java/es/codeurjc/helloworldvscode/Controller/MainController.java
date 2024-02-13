011f9347e9ecfe9cf4255fd12b264bbd8dbf6bff
aleromtor <alejandro.romero.torres@gmail.com>
2024-02-13 14:18:20 +0100

    Database entitys creations: -Exam -Subject -Student -Teacher
    RequestMapping for all htmls in controller

--- helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/Controller/MainController.java
+++ helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/Controller/MainController.java
@@ -19,4 +19,55 @@ public class MainController {
         return "main_page";
     }
 
+    @RequestMapping("/edit-profile")
+    public String editProfile() {
+        return "editProfile";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/main-page")
+    public String mainPage() {
+        return "main_page";
+    }
+
+    @RequestMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @RequestMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @RequestMapping("/subjects-registered-user")
+    public String subjectsRegisteredUser() {
+        return "subjects_registereduser";
+    }
+
+    @RequestMapping("/subject-info")
+    public String subjectInfo() {
+        return "subject_info";
+    }
+
+    @RequestMapping("/subject-one-subj-admin")
+    public String subjectOneSubjAdmin() {
+        return "subject_onesubj_admin";
+    }
+
+    @RequestMapping("/subject-one-subj-student")
+    public String subjectOneSubjStudent() {
+        return "subject_onesubj_student";
+    }
+
+    @RequestMapping("/subject-one-subj-teacher")
+    public String subjectOneSubjTeacher() {
+        return "subject_onesubj_teacher";
+    }
+
+
 }
