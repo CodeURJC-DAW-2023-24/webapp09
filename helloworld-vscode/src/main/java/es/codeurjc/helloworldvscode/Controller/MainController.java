3e2fb93a81405ced9a9977a52a2c9e9ccc16602a
javii <javiisalaas97@gmail.com>
2024-02-15 19:11:16 +0100

    fix packages

--- helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/Controller/MainController.java
+++ helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/Controller/MainController.java
@@ -1,34 +1,39 @@
-package es.codeurjc.helloworldvscode.Controller;
+package es.codeurjc.helloworldvscode.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MainController { 
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(HttpServletRequest request){
         return "main_page";
     }
+
     @RequestMapping("/subject") // LLEVARÁ UN ID DE ASIGNATURA 
     public String subject_student(HttpServletRequest request){
         return "subject_onesubj_student";
     }
+
     @RequestMapping("/subject-info") // LLEVARÁ UN ID DE ASIGNATURA 
     public String subject_info(HttpServletRequest request){
         return "subject_info";
     }
+
     @RequestMapping("/subjects") // LLEVARÁ UN ID DE USER 
     public String subjects_registereduser(HttpServletRequest request){
         return "subjects_registereduser";
     }
+
     @RequestMapping("/profile") // LLEVARÁ UN ID DE USER
     public String profile(HttpServletRequest request){
         return "profile";
     }
+
     @RequestMapping("/editProfile") // LLEVARÁ UN ID DE USER
     public String editProfile(HttpServletRequest request){
         return "editProfile";
@@ -38,7 +43,6 @@ public class MainController {
     public String editProfile() {
         return "editProfile";
     }
-
 
     @RequestMapping("/subject-one-subj-admin")
     public String subjectOneSubjAdmin() {
