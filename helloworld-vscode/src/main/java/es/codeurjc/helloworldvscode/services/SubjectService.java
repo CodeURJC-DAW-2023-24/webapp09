ce0db4a30df20252f906cb71efd5144205b2f986
javii <javiisalaas97@gmail.com>
2024-02-19 18:25:34 +0100

    asynchronous content loading main_page

--- helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/services/SubjectService.java
+++ helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/services/SubjectService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.helloworldvscode.Entitys.Subject;
@@ -15,11 +17,16 @@ public class SubjectService {
     @Autowired
     SubjectRepository subjectRepository;
 
-    public List<Subject> findAll(){
+    public List<Subject> findAll() {
         return subjectRepository.findAll();
     }
 
-    public Optional<Subject> unique(Long id){
+    public Optional<Subject> unique(Long id) {
         return subjectRepository.findSubjectById(id);
+    }
+
+    public List<Subject> getSubjects(Pageable pageable) {
+        Page<Subject> subjectPage = subjectRepository.findAll(pageable);
+        return subjectPage.getContent();
     }
 }
