e253ea734e16c1c01af4da723f9829f7b4390516
AmandaCastro <103392535+latinyloco@users.noreply.github.com>
2024-03-05 15:09:54 +0100

    Revert "Revert "Merge branch 'fix' of https://github.com/CodeURJC-DAW-2023-24/webapp09 into fix""
    This reverts commit 0d99fcb0bc9b6742808cd67a93e56454d2b79778.

--- helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/repository/SubjectRepository.java
+++ helloworld-vscode/src/main/java/es/codeurjc/helloworldvscode/repository/SubjectRepository.java
@@ -5,6 +5,7 @@ import es.codeurjc.helloworldvscode.Entitys.Subject;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     public Optional<Subject> findSubjectById(long id);
@@ -17,4 +18,7 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     // NO TIENE SENTIDO
     @SuppressWarnings("null")
     Optional<Subject> findById(Long subjectId);
+
+    @Query("SELECT s.gender, COUNT(s) FROM Subject s GROUP BY s.gender")
+    List<Object[]> countSubjectsByGender();
 }
